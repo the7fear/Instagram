bb0e845873665d03e110df58dd4cb556f6dc110d
Niko Kim <the.fear.dev@gmail.com>
2020-11-02 10:01:32 +0600

    Firebase SDK and User Registration

---
+++ Instagram_LBTA/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  Instagram_LBTA
+//
+//  Created by Niko Kim on 02.11.2020.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .blue
+  }
+}
--- Instagram_LBTA/ViewController.swift
+++ Instagram_LBTA/ViewController.swift
@@ -8,14 +8,40 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
   
   let plusPhotoButton: UIButton = {
     let button = UIButton(type: .system)
     button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
     button.layer.cornerRadius = 70
+    button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
     return button
   }()
+  
+  @objc func handlePlusPhoto() {
+    
+    let imagePickerController = UIImagePickerController()
+    imagePickerController.delegate = self
+    imagePickerController.allowsEditing = true
+    
+    present(imagePickerController, animated: true, completion: nil)
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    
+    if let editedImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage {
+      plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+    } else if let originalImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerOriginalImage")] as? UIImage {
+      plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+    plusPhotoButton.layer.masksToBounds = true
+    plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+    plusPhotoButton.layer.borderWidth = 3
+    
+    dismiss(animated: true, completion: nil)
+  }
   
   let emailTextField: UITextField = {
     let tf = UITextField()
@@ -82,7 +108,7 @@ class ViewController: UIViewController {
     
     plusPhotoButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: nil, paddingTop: 40, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 140, height: 140)
     plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-
+    
     setupInputFields()
   }
   
@@ -91,14 +117,53 @@ class ViewController: UIViewController {
     guard let email = emailTextField.text, email.count > 0 else { return }
     guard let username = usernameTextField.text, username.count > 0 else { return }
     guard let password = passwordTextField.text, password.count > 0 else { return }
-        
-    Firebase.Auth.auth().createUser(withEmail: email, password: password) { (result: AuthDataResult?, error: Error?) in
     
+    Firebase.Auth.auth().createUser(withEmail: email, password: password) { (result: AuthDataResult?, error: Error?) in
+      
       if let error = error {
         print("Failed to create user:", error)
       }
       
       print("Successfully created user:", result?.user.uid ?? "")
+      
+      guard let image = self.plusPhotoButton.imageView?.image else { return }
+      guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+      
+      let filename = NSUUID().uuidString
+      
+      let storageRef = Storage.storage().reference().child("profile_images").child(filename)
+      storageRef.putData(uploadData, metadata: nil) { (metadata, err) in
+        
+        if let err = err {
+          print("Failed to upload profile image:", err)
+          return
+        }
+        
+        storageRef.downloadURL { (downloadURL, err) in
+          if let err = err {
+            print("Failed to fetch downloadURL:", err)
+            return
+          }
+          
+          guard let profileImageUrl = downloadURL?.absoluteString else { return }
+          print("Successfully uploaded profile image:", profileImageUrl)
+          
+
+          guard let uid = result?.user.uid else { return }
+
+          let dictionaryValues = ["username": username, "profile_image": profileImageUrl]
+          let values = [uid: dictionaryValues]
+
+          Database.database().reference().child("users").updateChildValues(values) { (err, ref) in
+            if let err = err {
+              print("Failed to save user info into db:", err)
+              return
+            }
+
+            print("Successfully saved user into db")
+          }
+        }
+      }
     }
   }
   
@@ -110,9 +175,9 @@ class ViewController: UIViewController {
     stackView.axis = .vertical
     
     view.addSubview(stackView)
-//    NSLayoutConstraint.activate([
-//      stackView.heightAnchor.constraint(equalToConstant: 200)
-//    ])
+    //    NSLayoutConstraint.activate([
+    //      stackView.heightAnchor.constraint(equalToConstant: 200)
+    //    ])
     
     stackView.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 20, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 200)
   }
