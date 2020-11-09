4da87625f981e681fa8f79740fa08e17d31a3bc7
Niko Kim <the.fear.dev@gmail.com>
2020-11-09 12:37:53 +0600

    13. Fetch Photos Framework

---
+++ Instagram_LBTA/AddPhoto/PhotoSelectorCell.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoSelectorCell.swift
+//  Instagram_LBTA
+//
+//  Created by Niko Kim on 09.11.2020.
+//
+
+import UIKit
+
+class PhotoSelectorCell: UICollectionViewCell {
+  
+  let photoImageView: UIImageView = {
+    let iv = UIImageView()
+    iv.contentMode = .scaleAspectFill
+    iv.clipsToBounds = true
+    return iv
+  }()
+  
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    addSubview(photoImageView)
+    photoImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+}
--- Instagram_LBTA/AddPhoto/PhotoSelectorController.swift
+++ Instagram_LBTA/AddPhoto/PhotoSelectorController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
   
@@ -15,10 +16,39 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
   override func viewDidLoad() {
     super.viewDidLoad()
     collectionView.backgroundColor = .systemYellow
-    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    collectionView.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
     collectionView.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
     
     setupNavigationButtons()
+    fetchPhoto()
+  }
+  
+  var images = [UIImage]()
+  
+  fileprivate func fetchPhoto() {
+    
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.fetchLimit = 10
+    let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+    fetchOptions.sortDescriptors = [sortDescriptor]
+    let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+    allPhotos.enumerateObjects { (asset, count, stop) in
+      
+      let imageManager = PHImageManager.default()
+      let targetSize = CGSize(width: 350, height: 350)
+      let options = PHImageRequestOptions()
+      options.isSynchronous = true
+      
+      imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: options) { (image, info) in
+        if let image = image {
+          self.images.append(image)
+        }
+      }
+      
+      if count == allPhotos.count - 1 {
+        self.collectionView.reloadData()
+      }
+    }
   }
   
   override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -37,12 +67,13 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
   }
 
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    return images.count
   }
 
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-    cell.backgroundColor = .blue
+    let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PhotoSelectorCell
+    cell.photoImageView.image = images[indexPath.item]
+//    cell.backgroundColor = .blue
     return cell
   }
 
